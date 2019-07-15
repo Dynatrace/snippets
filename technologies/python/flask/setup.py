aa12bf657175814e0e974235959ca277dbe413dc
Sonja Chèvre <sonja.chevre@dynatrace.com>
2019-07-15 08:21:48 +0200

    Update setup.py
    update description

--- technologies/python/flask/setup.py
+++ technologies/python/flask/setup.py
@@ -1,18 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright 2019 Dynatrace LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import io
 
@@ -25,15 +11,15 @@ with io.open("README.rst", "rt", encoding="utf8") as f:
 setup(
     name="flaskr",
     version="1.0.0",
-    url="http://flask.pocoo.org/docs/tutorial/",
+    url="https://www.dynatrace.com/news/blog/monitoring-flask-applications",
     license="BSD",
-    maintainer="Pallets team",
+    maintainer="Pallets team (see http://flask.pocoo.org/docs/tutorial/), Dynatrace",
     maintainer_email="contact@palletsprojects.com",
-    description="The basic blog app built in the Flask tutorial.",
+    description="The basic blog app built in the Flask tutorial instrumented by Dynatrace OneAgent SDK.",
     long_description=readme,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask","oneagent-sdk","requests"],
+    install_requires=["flask","oneagent-sdk~=1.2","requests"],
     extras_require={"test": ["pytest", "coverage"]},
 )
